134b019929cf95edbfd5b059771679391bacc21c
Ruan Correia <correia.ruan@gmail.com>
2024-08-20 14:20:48 -0300

    Primeiro commit do projeto

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+# ENTRADA DE DADOS
+nome = input('Digite o nome do paciente: ')
+idade = int(input('Digite a idade do paciente: '))
+peso = int(input('Digite o peso registrado: '))
+altura = float(input('Digite a altura do paciente: '))
+cintura = float(input('Digite a circunferencia da cintura: '))
+porcentage_gordura = input('Qual método está utilizando para medição? 1- Adipômetro / 2- Bioimpendâcia')
+if(porcentagem_gordura == 1):
+  porcentagem_gordura = DC (g/cm³) =1,112-0.00043499 x (soma 7 Dobras) + 0.00000055 x (soma 7 Dobras) x 2 – 0.00028826 x (Idade).
+elif(porcentagem_gordura == 2):
+  porcentagem_gordura = MLG = 0,73 (estatura²/resistência) + 0,116 (massa corporal) + 0.096 (reatância) – 4.03
+
+
+# CALCULOS
+
+
+
