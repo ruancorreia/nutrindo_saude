9346b97df39e6f924686ebd7872affb41991f637
Ruan Correia <correia.ruan@gmail.com>
2024-08-23 17:14:19 -0300

    laços por idade

--- calculadora.py
+++ calculadora.py
@@ -37,8 +37,68 @@ elif(metodo_medicao == 2):
   else:
     print('Valores incorretos! Digite novamente.')
 
+# IMC ADULTOS / IDOSOS
+  if idade <=59:
+    if imc <= 18.49:
+      print('Abaixo do Peso!')
+    elif imc >= 18.5 and imc <= 24.99:
+      print('Imc Adequado!')
+    elif imc >= 25 and 29.99:
+      print('Sobrepeso!')
+    elif imc >= 30 and imc <= 34.99:
+      print('Obesidade grau I')
+    elif imc >= 35 and imc <= 39.99:
+      print('Obesidade Grau II')
+    else:
+      print('Obesidade Grau III')
+  else:
+    if imc <= 22:
+      print('Baixo Peso')
+    elif imc >= 22.01 and imc >= 26.99:
+      print('Adequado!')
+    else:
+      print('Sobrepeso!')
+  
+# Circunferencia da cintura
+if sexo == 0:
+  if cintura <= 79:
+    print('Circunferencia da cintura adequada')
+  else:
+    print('Circunferencia da cintura elevada!')
+elif sexo == 1:
+  if cintura <= 93:
+    print('Circunferencia da cintura adequada!')
+  else:
+    print('Circunferencia da cintura elevada!')
+else:
+  print('Dados Inválidos!')
 
-
-
+# Porcentagem de gordura corporal
+if sexo == 0:
+  if idade >= 20 and idade <= 39:
+    if porcentagem_gordura <= 21:
+      print('Porcentagem de gordura baixa!')
+    elif porcentagem_gordura >= 21.1 and porcentagem_gordura <= 32.9:
+      print('porcentagem de gordura adequada!')
+    else:
+      print('Porcentagem de gordura alta!')
+  elif idade >= 40 and idade <= 59:
+    if porcentagem_gordura <= 23:
+      print('Porcentagem de gordura baixo!')
+    elif porcentagem_gordura <= 23.1 and porcentagem_gordura >= 33.9:
+      print('Porcentagem de gordura adequada!')
+    else:
+      print('Porcentagem de gordura Alta!')
+  elif idade >= 60 and idade <= 79:
+      if porcentagem_gordura <= 24:
+        print('Porcentagem de gordura baixa!')
+      elif porcentagem_gordura >= 24.1 and porcentagem_gordura <= 35.9:
+        print('Porcentagem de gordura adequada!')
+      else:
+        print('Porcentagem de gordura alta!')
+elif sexo == 1:
+  if idade >= 20 and idade <= 39:
+    if porcentagem_gordura <= 8:
+      print('')
 
 
